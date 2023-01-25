5c23098dd28f54e13ef65b7afda4ad108cf3c5bd
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-25 22:01:14 +0300

    #504861 #385054

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -36,44 +36,4 @@ public class MemoryCandidateRepository  implements CandidateRepository {
     }
 
 
-//    private int nextId = 1;
-//
-//    private final Map<Integer, Candidate> candidates = new HashMap<>();
-//
-//    public MemoryCandidateRepository() {
-//        save(new Candidate(0, "Иванов Иван Петрович","Хороший человек",  LocalDateTime.of(2023,
-//                Month.JANUARY, 01, 15, 30, 00)));
-//        save(new Candidate(0, "Иванов Иван Петрович2","Хороший человек2",  LocalDateTime.of(2023,
-//                Month.JANUARY, 01, 15, 30, 00)));
-//
-//    }
-//
-//
-//    @Override
-//    public Candidate save(Candidate candidate) {
-//        candidate.setId(nextId++);
-//        candidates.put(candidate.getId(), candidate);
-//        return candidate;
-//    }
-//
-//    @Override
-//    public boolean deleteById(int id) {
-//        return candidates.remove(id) != null;
-//    }
-//
-//    @Override
-//    public boolean update(Candidate candidate) {
-//        return candidates.computeIfPresent(candidate.getId(), (id, oldCandidate) -> new Candidate(oldCandidate.getId(),  candidate.getName(), candidate.getDescription(),candidate.getCreationDate())) != null;
-//    }
-//
-//    @Override
-//    public Optional<Candidate> findById(int id) {
-//        return Optional.ofNullable(candidates.get(id));
-//    }
-//
-//    @Override
-//    public Collection<Candidate> findAll() {
-//        return candidates.values();
-//    }
-
 }
