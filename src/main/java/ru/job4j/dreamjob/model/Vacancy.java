459bdacd09e67cff1e188e43adc35833f8895ab7
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-25 19:54:12 +0300

    #504854

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -9,16 +9,26 @@ public class Vacancy {
     private String title;
     private String description;
     private boolean visible;
+    private int cityId;
     private LocalDateTime creationDate = LocalDateTime.now();
 
     public Vacancy() {
     }
-    public Vacancy(int id, String title , String description, LocalDateTime creationDate, boolean visible) {
+    public Vacancy(int id, String title , String description, LocalDateTime creationDate, boolean visible, int cityId) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.creationDate = creationDate;
         this.visible = visible;
+        this.cityId = cityId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
     }
     public boolean getVisible() {
         return visible;
--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -20,17 +20,17 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     public MemoryVacancyRepository() {
         save(new Vacancy(0, "Intern Java Developer", "Intern Java Developer", LocalDateTime.of(2023,
-                Month.JANUARY, 01, 15, 30, 00), true));
+                Month.JANUARY, 01, 15, 30, 00), true, 1));
         save(new Vacancy(0, "Junior Java Developer", "Junior Java Developer", LocalDateTime.of(2023,
-                Month.JANUARY, 01, 15, 30, 00), true));
+                Month.JANUARY, 01, 15, 30, 00), true, 1));
         save(new Vacancy(0, "Junior+ Java Developer", "Junior+ Java Developer", LocalDateTime.of(2023,
-                Month.JANUARY, 01, 15, 30, 00), true));
+                Month.JANUARY, 01, 15, 30, 00), true, 1));
         save(new Vacancy(0, "Middle Java Developer", "Middle Java Developer", LocalDateTime.of(2023,
-                Month.JANUARY, 01, 15, 30, 00), true));
+                Month.JANUARY, 01, 15, 30, 00), true, 1));
         save(new Vacancy(0, "Middle+ Java Developer", "Middle+ Java Developer", LocalDateTime.of(2023,
-                Month.JANUARY, 01, 15, 30, 00), true));
+                Month.JANUARY, 01, 15, 30, 00), true, 1));
         save(new Vacancy(0, "Senior Java Developer", "Senior Java Developer", LocalDateTime.of(2023,
-                Month.JANUARY, 01, 15, 30, 00), true));
+                Month.JANUARY, 01, 15, 30, 00), true, 1));
     }
 
 
@@ -49,7 +49,7 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     @Override
     public boolean update(Vacancy vacancy) {
-        return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getCreationDate(), vacancy.getVisible())) != null;
+        return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getCreationDate(), vacancy.getVisible(), vacancy.getCityId())) != null;
     }
 
     @Override
